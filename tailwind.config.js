4ab9105794f39e58e9ca723b0700ab4218a5701f
nnaimzoda6 <naimzoda07@gmal.com>
2023-03-10 19:27:28 +0500

    polni full praect niyozmuhammad

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,20 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     screens: {
-      phone:{max : "500px" },
-      phone2:{max :"700px"}
+      '2xl': {'max': '1535px'},
+      // => @media (max-width: 1535px) { ... }
+
+      'xl': {'max': '1279px'},
+      // => @media (max-width: 1279px) { ... }
+
+      'lg': {'max': '1023px'},
+      // => @media (max-width: 1023px) { ... }
+
+      'md': {'max': '767px'},
+      // => @media (max-width: 767px) { ... }
+
+      'sm': {'max': '400px'},
+      // => @media (max-width: 639px) { ... }
      
     },
   },
