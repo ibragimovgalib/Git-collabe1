2effb4763da2243631140205eb4d1caf8a68ef6a
nnaimzoda6 <naimzoda07@gmal.com>
2023-03-10 20:00:10 +0500

    meshiday da

--- tailwind.config.js
+++ tailwind.config.js
@@ -17,10 +17,6 @@ module.exports = {
 
       'sm': {'max': '500px'},
       // => @media (max-width: 639px) { ... }
-     
-      phone500: {max: '500px' },
-      phone700: {min: '501px', max: '700px' },
-      phone1000: {min: '701px', max: '1700px' },
     },
   },
   plugins: [],
